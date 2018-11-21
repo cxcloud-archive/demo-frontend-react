6f07341fde66e1928bde9ffceab20f07ae8fb4ad
mpxy <mpxy03@gmail.com>
2018-11-21 16:10:58 +0200

    Moved global styles to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,39 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import { Colors } from '../common/theme';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
   render() {
     return (
       <html>
         <Head>
-          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-          <style>{`body { margin: 0 } /* custom! */`}</style>
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" defer></script>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <style global jsx>{`
+            body {
+              margin: 0;
+              font-family: Helvetica, Geneva, Tahoma, sans-serif;
+            }
+            body,
+            a {
+              color: ${Colors.darkSlateGray};
+            }
+          `}</style>
+          <script
+            src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+            defer
+          />
         </Head>
-        <body className="custom_class">
+        <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
