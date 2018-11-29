54929394f0711e9e66df96692bea7df9d74d6833
mpxy <mpxy03@gmail.com>
2018-11-29 20:17:19 +0200

    Refactored CategoryList component

--- components/CategoriesList.js
+++ components/CategoriesList.js
@@ -2,26 +2,12 @@ import CategoryCard from './CategoryCard';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  margin: 1.875rem auto;
-  padding: 1.25rem 2.5rem;
-  @media only screen and (max-width: 400px) {
-    padding: 0 1.25rem;
-  }
-`;
-const ListOfCategories = styled.div`
-  margin: 0 auto 2em auto;
   max-width: 100em;
-  padding-bottom: 5em;
   display: flex;
   flex-direction: row;
   justify-content: center;
   flex-wrap: wrap;
-  :active {
-    color: rgba(211, 183, 86, 1);
-  }
-  :nth-child(2n) {
-    flex-grow: 1;
-  }
+
   @media only screen and (max-width: 1600px) {
     max-width: 65em;
   }
@@ -29,10 +15,8 @@ const ListOfCategories = styled.div`
 
 export default ({ categories = [] }) => (
   <Wrapper>
-    <ListOfCategories>
-      {categories.map((category, i) => (
-        <CategoryCard key={i} id={category.id} name={category.name.en} />
-      ))}
-    </ListOfCategories>
+    {categories.map((category, i) => (
+      <CategoryCard key={i} id={category.id} name={category.name.en} />
+    ))}
   </Wrapper>
 );
