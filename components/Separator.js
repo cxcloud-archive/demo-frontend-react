3b4d8780ab9b72ef5ef69d34892e9790e620e31c
mpxy <mpxy03@gmail.com>
2018-11-29 20:18:45 +0200

    Created Separator component

---
+++ components/Separator.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Colors } from '../common/theme';
+
+export default styled.hr`
+  width: 100%;
+  margin: 1rem 0;
+  border: 0;
+  border-top: 2px solid ${Colors.concrete};
+`;
