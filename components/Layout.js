206d89f00f1bb37b51546ea467d5081ab6fea0dd
mpxy <mpxy03@gmail.com>
2018-12-11 10:53:28 +0200

    Upgraded next.js, added css support and normalize.css

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../node_modules/normalize.css/normalize.css';
 
 export default ({ children }) => (
   <div>
