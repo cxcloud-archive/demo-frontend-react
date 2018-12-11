c75d36922c4ae3460e4e7b6ff820802d22b35a8d
mpxy <mpxy03@gmail.com>
2018-12-11 16:19:04 +0200

    Refactored product card styles

--- components/ProductsCard.js
+++ components/ProductsCard.js
@@ -4,40 +4,43 @@ import { Colors } from '../common/theme';
 
 const Wrapper = styled.div`
   display: flex;
-  align-items: flex-end;
-  justify-content: left;
-  margin: 1.25rem 0rem;
-  padding: 1.25rem 1.87rem;
-  max-width: 16em;
-  min-height: 24em;
-  margin: 1.25rem;
-  width: 100%;
-  font-weight: bold;
+  flex: 1 1 12rem;
+  max-width: 13em;
+  padding: 1.25rem;
+  margin: 0.75rem;
   box-shadow: inset 0rem 0rem 0.81rem -0.31rem ${Colors.darkSlateGray};
-  border-radius: 0.18rem;
+  border-radius: 0.2rem;
   a {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
     width: 100%;
-    text-decoration: none;
-    font-size: 0.87rem;
-    font-weight: 500;
     text-transform: capitalize;
-    .description {
-      display: flex;
-      flex-direction: column;
-    }
+  }
+`;
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  p {
+    margin-top: 0;
+    font-weight: 100;
+    font-size: 0.87rem;
   }
 `;
 const Title = styled.h1`
-  font-weight: bold;
-  font-size: 1.25rem;
+  font-weight: 400;
+  font-size: 1.1rem;
 
   ${Wrapper}:hover & {
     color: ${Colors.lightGray};
   }
 `;
-const Price = styled.p`
+const Price = styled.span`
   padding-top: 0.62rem;
   border-top: 0.12rem solid rgba(204, 204, 204, 0.4);
+  font-weight: 400;
 `;
 
 export default ({ id, name, image, description, price, currency }) => {
@@ -47,14 +50,14 @@ export default ({ id, name, image, description, price, currency }) => {
     <Wrapper>
       <Link href={`/ProductPage?id=${id}`}>
         <a>
-          <img src={image} width="180" height="240" alt="" />
-          <div className="description">
+          <img src={image} width="100%" height="auto" alt="" />
+          <Description>
             <Title>{name}</Title>
             <p>{description}</p>
-            <Price>
-              {priceInEuro} {currency}
-            </Price>
-          </div>
+          </Description>
+          <Price>
+            {priceInEuro} {currency}
+          </Price>
         </a>
       </Link>
     </Wrapper>
