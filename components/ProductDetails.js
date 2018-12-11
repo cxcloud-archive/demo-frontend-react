236738014fc99d9968e7f6c45d7e8f3a78f01c5c
mpxy <mpxy03@gmail.com>
2018-12-11 16:18:32 +0200

    Refactored product details styles

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -4,32 +4,28 @@ import { faStar } from '@fortawesome/fontawesome-free-solid';
 import { Colors } from '../common/theme';
 
 const Wrapper = styled.div`
-  margin: 10em auto auto auto;
   display: flex;
-  justify-content: center;
-  padding: 1.25rem 2.5rem;
-  text-transform: capitalize;
-`;
-const ProductContainer = styled.div`
-  display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
+  text-transform: capitalize;
   .price {
     font-weight: bold;
   }
-  @media only screen and (max-width: 580px) {
-    display: flex;
-    flex-direction: column;
+  img {
+    max-height: 0rem;
   }
-`;
-const ProductImg = styled.div`
-  text-align: center;
-  max-width: 30em;
+  @media only screen and (max-width: 580px) {
+    flex-direction: column;
+    img {
+      max-height: 20rem;
+    }
+  }
 `;
 const ProductInfo = styled.div`
   max-width: 50em;
   @media only screen and (max-width: 1100px) {
-    padding: 0 2.5rem;
+    margin-left: 2.5rem;
     line-height: 1.62;
   }
   h1 {
@@ -38,65 +34,50 @@ const ProductInfo = styled.div`
     color: darkslategray;
   }
 `;
-const Rating = styled.div`
-  display: inline-flex;
+const Rating = styled.p`
+  display: flex;
   align-items: center;
-  p {
+  a {
     margin-left: 0.93rem;
-    a {
-      color: lightgray;
-      :visited {
-        color: #000000;
-      }
+    color: lightgray;
+    :visited {
+      color: #000000;
     }
   }
 `;
-const Cart = styled.div`
-  button {
-    padding: 0.93rem 1.25rem;
-    border-radius: 0.12rem;
-    background-color: #fff;
-    color: ${Colors.mustard};
-    font-size: 1rem;
-    font-weight: 600;
-    text-transform: uppercase;
+const CartButton = styled.button`
+  cursor: pointer;
+  padding: 0.5em 1em;
+  background-color: ${Colors.mustard};
+  color: white;
+  font-size: 0.9rem;
+  text-transform: capitalize;
+  &:hover {
+    border-color: transparent;
     box-shadow: inset 0rem 0rem 0.5rem -0.18rem ${Colors.darkSlateGray};
-    :hover {
-      border-color: transparent;
-      background-color: rgba(89, 85, 85, 0.1);
-      color: inherit;
-    }
   }
 `;
 
 export default ({ name, image, priceInEuro, sku, color, size }) => (
   <Wrapper>
-    <ProductContainer>
-      <ProductImg>
-        <img height="640" width="480" src={image} alt="blank" />
-      </ProductImg>
-      <ProductInfo>
-        <div>
-          <h1>{name}</h1>
-          <p>{sku}</p>
-        </div>
-        <Rating>
-          <FontAwesomeIcon icon={faStar} />
-          <FontAwesomeIcon icon={faStar} />
-          <FontAwesomeIcon icon={faStar} />
-          <FontAwesomeIcon icon={faStar} />
-          <FontAwesomeIcon icon={faStar} />
-          <p>
-            <a href="">Write first review</a>
-          </p>
-        </Rating>
-        <p>Color: {color}</p>
-        <p>Size: {size}</p>
-        <p className="price">€ {priceInEuro}</p>
-        <Cart>
-          <button>Add to cart</button>
-        </Cart>
-      </ProductInfo>
-    </ProductContainer>
+    <img src={image} alt="blank" />
+    <ProductInfo>
+      <div>
+        <h1>{name}</h1>
+        <p>{sku}</p>
+      </div>
+      <Rating>
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <a href="">Write first review</a>
+      </Rating>
+      <p>Color: {color}</p>
+      <p>Size: {size}</p>
+      <p className="price">€ {priceInEuro}</p>
+      <CartButton>Add to cart</CartButton>
+    </ProductInfo>
   </Wrapper>
 );
